357bf40e92f8d55761922f6c42010309a09ee9bd
github username <github email address>
2021-06-20 21:33:45 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", () => {getBooks()});
+
+const getBooks = () => {
+    fetch(`http://localhost:3000/books`)
+    .then(res => res.json())
+    .then(books => books.forEach(renderBookList))
+}
+
+const renderBookList = (book) => {
+    const sideBookList = document.getElementById("list")
+
+    const bookList = document.createElement('li')
+    bookList.innerHTML = book.title
+
+    sideBookList.append(bookList)
+
+    bookList.addEventListener('click', () => {showBookInfo(book)})
+}
+
+const showBookInfo = (book) => {
+
+    const bookImage = document.createElement('img')
+    bookImage.src = book.img_url
+
+    const bookTitle = document.createElement('h2')
+    bookTitle.innerText = book.title
+
+    const bookAuthor = document.createElement('h2')
+    bookAuthor.innerText = book.author
+
+    const bookSubtitle = document.createElement('h2')
+    bookSubtitle.innerText = book.subtitle
+
+    const bookSummary = document.createElement('p')
+    bookSummary.innerText = book.description
+
+    const bookUsers = (book.users.username)
+    console.log(bookUsers)
+
+    const bookLikes = document.createElement('li')
+    bookLikes.innerHTML = bookUsers
+
+    // const bookUsers = book.filter(book.user.username)
+    // console.log(bookUsers)
+
+
+
+    const bookDisplay = document.getElementById("show-panel")
+    bookDisplay.innerHTML = ''
+    bookDisplay.append(bookImage, bookTitle, bookSubtitle, bookAuthor, bookSummary, bookLikes)
+}
